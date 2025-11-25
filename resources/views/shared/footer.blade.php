ba2ca393018556279afe7c232f0ca6a1d215edcf
AgungPajar <presetpjr@gmail.com>
2025-11-26 06:15:15 +0700

    Fix formatting in footer: remove extra whitespace and ensure newline at end of file

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="footer-content">
         <img src="{{ asset('logocreate.png') }}" alt="Logo" class="footer-logo">
         <p class="footer-text">
-            © OSSAGAR 24 | Created by 
+            © OSSAGAR 24 | Created by
             <a href="https://www.gncs.dev/" target="_blank" rel="noopener noreferrer" class="footer-link">GnC Team</a>
         </p>
     </div>
