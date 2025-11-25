f85d6c0946e8a811479c63fcbb8ea30068353c48
Rafpelmln <muhammadfarrelmaulana07@gmail.com>
2025-11-25 18:35:16 +0700

    Add additional links and credits to the thanks page

--- resources/views/voter/thanks.blade.php
+++ resources/views/voter/thanks.blade.php
@@ -9,5 +9,7 @@
         <p>Suaramu untuk <strong>{{ $paslonName }}</strong> telah direkam dengan aman.</p>
         <p class="muted">Silakan kembali ke halaman login jika ingin membantu teman lain untuk memilih dengan token berbeda.</p>
         <a href="{{ route('voter.login') }}" class="primary-button mt">Kembali ke Login</a>
+        <a href="https://gncs.dev" target="_blank" rel="noopener noreferrer" class="primary-button mt">Lihat Tentang Kami</a>
+        <p class="muted mt">Created by GNCS x PPLG</p>
     </div>
 @endsection
